b042436327f7e5f2fee168b083e883b77991422b
ndiediop <ndieyed48@gmail.com>
2026-05-06 19:02:32 +0000

    correction entity Paiement

---
+++ NAttFlow/Entities/Paiement.cs
@@ -0,0 +1,15 @@
+namespace NattFlow.Entities
+{
+    public class Paiement
+    {
+        public int IdPaiement { get; set; }
+        public int Montant { get; set; }
+        public DateTime DatePaiement { get; set; }
+        public string Method { get; set; } = string.Empty;
+        public string Statut { get; set; } = string.Empty;
+        public int IdCotisation { get; set; }
+        public Cotisation Cotisation { get; set; } = null!;
+        public int IdUser { get; set; }
+        public User User { get; set; } = null!
+    }
+}
